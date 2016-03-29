219a2767d15c5635ebdd07f7be94b33cd03d41c5
mckconsult <mckconsult@yandex.ru>
2016-03-29 18:58:40 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
 <head>
-   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+   <meta http-equiv="Content-Type" content="text/html; charset=utf8" />
    <title>Ваше сообщение успешно отправлено</title>
 </head>
  
@@ -19,7 +19,7 @@
  
       mail('visitka36@mail.com', 'Заказ звонка', 
       'Вам написал: '.$name.'<br />Его номер: '.$phone.'<br />Его почта: '.$mail.'<br />
-      Его сообщение: '.$message,"Content-type:text/html;charset=utf-8");
+      Его сообщение: '.$message,"Content-type:text/html;charset=utf8");
  
       echo "Ваше сообщение успешно отправлено!<Br> Вы получите ответ в 
       ближайшее время<Br> $back";
